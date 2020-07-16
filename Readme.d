ec4f7f8d5ef21a1770afba0feff7712d81fee49f
Yo <nix@yo.local>
2020-07-16 10:54:40 +0600

    init

---
+++ Readme.d
@@ -0,0 +1,3 @@
+# Auto sync(aka commit) on changes (in a directory) to your git repo.
+
+You can run other commands too!
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import hel "github.com/thejini3/go-helper"
+
+func main() {
+	hel.Pl("Starting git-sync")
+}
